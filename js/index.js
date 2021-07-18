0d75c86e92db6a29318f55143d585ebf58efb98a
Maureen Njihia <maureennjihia468@gmail.com>
2021-07-19 00:32:28 +0300

    Add akan Names to respective arrays with days of the week

--- js/index.js
+++ js/index.js
@@ -6,3 +6,32 @@ let akanFemale=[];
 
 //Array to take in male akan names
 let akanMale=[];
+
+inputDay = Number(document.getElementById("day").value);
+inputMonth = Number(document.getElementById("month").value);
+inputYear = Number(document.getElementById("year").value);
+gender = document.querySelector('input[name= "gender"]:checked');
+message = document.querySelector(".error");
+successMessage = document.querySelector(".div-alert");
+
+//Female Akan Names
+let akanFemale = [
+    {dayOfTheWeek: "Sunday", akanName: "Akosua"},
+    {dayOfTheWeek: "Monday", akanName: "Adwoa"},
+    {dayOfTheWeek: "Tuesday", akanName: "Abenaa"},
+    {dayOfTheWeek: "Wednesday", akanName: "Akua"},
+    {dayOfTheWeek: "Thursday", akanName: "Yaa"},
+    {dayOfTheWeek: "Friday", akanName: "Afua"},
+    {dayOfTheWeek: "Saturday", akanName: "Ama"},
+];
+
+//Male Akan Names
+let akanMale = [
+    {dayOfTheWeek: "Sunday", akanName: "Kwasi"},
+    {dayOfTheWeek: "Monday", akanName: "Kwadwo"},
+    {dayOfTheWeek: "Tuesday", akanName: "Kwabena"},
+    {dayOfTheWeek: "Wednesday", akanName: "Kwaku"},
+    {dayOfTheWeek: "Thursday", akanName: "Yaw"},
+    {dayOfTheWeek: "Friday", akanName: "Kofi"},
+    {dayOfTheWeek: "Saturday", akanName: "Kwame"},
+];

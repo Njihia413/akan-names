2c6ceed7e15d3dd8e501239c5d4455ab4302cb5e
Maureen Njihia <maureennjihia468@gmail.com>
2021-07-19 00:10:34 +0300

    Declared required variables and arrays

---
+++ js/index.js
@@ -0,0 +1,8 @@
+//Initialising variables needed
+let inputDay,inputMonth, inputYear, gender, message, successMessage;
+
+//Array to take in female akan names
+let akanFemale=[];
+
+//Array to take in male akan names
+let akanMale=[];
